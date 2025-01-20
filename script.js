74dc6f47e546f3b87feadd64d343851761db7341
BernardoCastello <becastellosilva@gmail.com>
2025-01-20 09:36:15 -0300

    Adição de conhecimentos e mudança no texto

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function changeLanguage(lang) {
     const languageElements = {
         'pt-br': {
-            'bio-text': 'Olá, meu nome é <span class="highlight">Bernardo Castello</span> e sou estudante de <span class="highlight">Engenharia de Computação</span> na <span class="highlight">FURG</span> e estou no 4° ano do curso.',
+            'bio-text': 'Olá, meu nome é <span class="highlight">Bernardo Castello</span> e sou graduado em <span class="highlight_negrito">Engenharia de Computação</span></a> na <a href="https://www.furg.br/" target="_blank"><span class="highlight_negrito">FURG</span></a>.</p>',
             'welcome-text': 'Seja bem-vindo!',
             'title': '<span class="highlight">Engenheiro de Computação</span>',
-            'text_about': ' Olá, me chamo Bernardo e fui formado na <a href="https://www2.fab.mil.br/epcar/" target="_blank"<span class="highlight">Escola Preparatória de Cadetes do Ar(EPCAr)</span></a>, atualmente sou estudante de <a href="http://www.ecomp.c3.furg.br/" target="_blank"><span class="highlight">Engenharia de Computação</span></a> pela <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a>. Possuo experiência em ciência e análise de dados juntamente com deep learning e machine learning. Agora estou ampliando meus horizontes para o desenvolvimento web também, para assim desenvolver e ampliar meus conhecimentos.',
+            'text_about': ' Olá, me chamo Bernardo e me formei na <a href="https://www2.fab.mil.br/epcar/" target="_blank"><span class="highlight">Escola Preparatória de Cadetes do Ar(EPCAr)</span></a> e possuo bacharelado em <span class="highlight">Engenharia de Computação</span></a> pela <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a>. Possuo Conhecimentos em ciência e análise de dados, previsão de dados com machine learning, utilização de LLMs para chatbots e consulta de documentos, juntamente com tecnologias em nuvem. Também possuo experiência em criação de aplicativos com Unity para óculos de realidade virtual.<br><br>Possuo experiências através de bolsas de pesquisa em iniciação científica pela <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a> e pelo <a href="https://itecfurg.org/" target="_blank"><span class="highlight">Itec</span></a> e também trabalhando na empresa <a href="https://augenengenharia.com/" target="_blank"><span class="highlight">Augen Engenharia S.A.</span></a> como desenvolvedor.',
             'btn-projects': '<ion-icon name="logo-github"></ion-icon><span>Ver Projetos</span>',
-            'skills-section-title': 'Meus Conhecimentos:'
+            'skills-section-title': 'Meus Conhecimentos:',
+            'atualizado': 'Atualizado em 20/01/2025'
             // Adicione outras traduções para os elementos que precisam ser traduzidos
         },
         'en': {
-            'bio-text': 'Hello, my name is <span class="highlight">Bernardo Castello</span> and I am a student of <span class="highlight">Computer Engineering</span> at <span class="highlight">FURG</span> and I am in the 4th year of the course.',
+            'bio-text': 'Hello, my name is <span class="highlight">Bernardo Castello</span> and I have a degree in <span class="highlight_negrito">Computer Engineering</span></a> from <a href="https://www.furg.br/" target="_blank"><span class="highlight_negrito">FURG</span></a>.</p>',
             'welcome-text': 'Welcome!',
             'title': '<span class="highlight">Computer Engineer</span>',
-            'text_about': 'Hello, my name is Bernardo and I graduated from <a href="https://www2.fab.mil.br/epcar/" target="_blank"<span class="highlight">Escola Preparatória de Cadetes do Ar (EPCAr )</span></a>, I am currently a student of <a href="http://www.ecomp.c3.furg.br/" target="_blank"><span class="highlight">Engineering Computing</span></a> by <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a>. I have experience in data science and analysis along with deep learning and machine learning. Now I´m expanding my horizons to web development as well, to develop and expand my knowledge.',
+            'text_about': 'Hello, my name is Bernardo and I graduated from <a href="https://www2.fab.mil.br/epcar/" target="_blank"><span class="highlight">Escola Preparatória de Cadetes do Ar (EPCAr)</span></a> and have a bachelor\'s degree in <span class="highlight">Computer Engineering</span></a> from <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a>. I have knowledge in data science and analysis, data forecasting with machine learning, use of LLMs for chatbots and document consultation, together with cloud technologies. I also have experience in creating applications with Unity for virtual reality glasses.<br><br> I have experience through research grants in scientific initiation by <a href="https://www.furg.br/" target="_blank"><span class="highlight">Furg</span></a> and by <a href="https://itecfurg.org/" target="_blank"><span class="highlight">Itec</span></a> and also working at the company <a href="https://augenengenharia.com/" target="_blank"><span class="highlight">Augen Engenharia S.A.</span></a> as a developer.',
             'btn-projects': '<ion-icon name="logo-github"></ion-icon><span>See Projects</span>',
-            'skills-section-title': 'My Knowledge:'
+            'skills-section-title': 'My Knowledge:',
+            'atualizado': 'Updated on 20/01/2025'
             // Adicione outras traduções para os elementos que precisam ser traduzidos
         }
     };
